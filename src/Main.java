72e8862ff9589b4c34aacf0529c83fe147b14046
Black-and-white-Friday13 <vFriday13v@gmail.com>
2016-10-06 01:40:13 +0300

    change printList

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException
+    {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             ArrayList<Integer> newList  = new ArrayList<Integer>();
             ArrayList<Integer> newList1  = new ArrayList<Integer>();
@@ -17,24 +17,29 @@ public class Main {
             {
                 newList.add(Integer.parseInt(reader.readLine()));
             }
-            for (int a : newList){
-                if (a % 3 == 0){
+            for (int a : newList)
+            {
+                if (a % 3 == 0)
+                {
                     newList1.add(a);
                 }
-                else if (a % 2 == 0){
+                else if (a % 2 == 0)
+                {
                     newList2.add(a);
                 }
-                else {
+                else
+                {
                     newList3.add(a);
                 }
             }
-
-        }
-
-    public static void printList(List<Integer> list) {
-
-
-
+                printList(newList1);
+                printList(newList2);
+                printList(newList3);
     }
+
+    public static void printList(List<Integer> list)
+    {
+        System.out.println(list);
     }
+}
 
